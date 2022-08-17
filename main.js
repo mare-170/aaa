c7a16af320ea28231304d465b018683f5273c7dc
mare-170 <111253886+mare-170@users.noreply.github.com>
2022-08-18 05:45:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-var name = Element(id=name)
+window.onload = function() {
+    document.getElementById('calc_button').onclick = function() {
+        post();
+    };
+
+
+xhr = new XMLHttpRequest();
+
+xhr.onload = function (e) {
+    if (xhr.readyStata === 4) {
+        if (xhr.status === 200) {
+        var answer = document.getElementById('answer');
+        answer.value = xhr.responseText;
+    }
+    }
+
+};
+};
+
+function post() {
+    xhr.open('POST', 'calc.php', true);
+    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+
+    var request = "arg1=" + arg1.value + "&arg2=" + arg2.value;
+    xhr.send(request);
+
+}
